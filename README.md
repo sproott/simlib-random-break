ae35573a59a216ce2b7484ac7eeaee9a0f18f474
sproott <david.hrabe12@seznam.cz>
2023-12-02 18:23:03 +0100

    feat: better explanation and output

---
+++ README.md
@@ -0,0 +1,5 @@
+This minimal example showcases the `Random()` function from SIMLIB returning 1.
+
+This happens after 113720 iterations with a starting seed of 754494563.
+
+The last working seed before the broken one is 163364931. The next seed comes out as 2147483647 (`MAXLONGINT`), therefore generating the number 1.
--- main.cpp
+++ main.cpp
@@ -2,16 +2,30 @@
 #include <iostream>
 
 int main(int argc, char *argv[]) {
-  RandomSeed(754494563);
+  long seed = 754494563L;
+
+  std::cout << "Starting with seed: " << seed << std::endl;
+
+  RandomSeed(seed);
 
   size_t i = 0;
   double random;
-  do {
+  long currentSeed = seed;
+  for (;;) {
     random = Random();
+
+    if (random == 1) {
+      break;
+    }
+    currentSeed *= 1220703125L; // MULCONST
+    currentSeed &= 2147483647L; // MAXLONGINT
     i++;
-  } while (random != 1);
+  }
+
   std::cout << "Broken after " << i << " iterations, Random() == 1"
             << std::endl;
+
+  std::cout << "Last working seed: " << currentSeed << std::endl;
 
   return 0;
 }
